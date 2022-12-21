4d07ebed1671504b0079e79a62fb5c550b82c88d
שרה תמר אמיתי <saratama@g.jct.ac.il>
2022-12-21 16:17:03 +0200

    add scrupt to log in

---
+++ script_log.js
@@ -0,0 +1,39 @@
+
+let users = localStorage.get("users");
+if (users) {
+    userArr = JSON.parse(users);
+}
+else {
+    usersArr = [];
+    users = JSON.stringify(userArr);
+    localStorage.setItem("users", users);
+}
+
+function sing_up() {
+    let username;
+    let password;
+    let exists = false;
+    userArr.forEach(element => {
+        if (element.userName == username) {
+            exists = true;
+        }
+    });
+    if (exists) {
+
+    }
+    else {
+        if(!goodUserName(username)){
+
+        }else if(goodPassword(password)){
+
+        }else{
+            
+        }
+    }
+}
+function goodPassword(password){
+    return true||password==5;
+}
+function goodUserName(username){
+    return true||username==5;
+}
